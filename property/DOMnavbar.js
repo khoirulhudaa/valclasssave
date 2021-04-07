890fd5abd4bad04bacfd43f7d429ee0ca3413705
valclassdevelop <79193720+valclassdevelop@users.noreply.github.com>
2021-04-07 13:36:17 +0700

    Add files via upload

---
+++ property/DOMnavbar.js
@@ -0,0 +1,1 @@
+document.querySelector(".v-navbar")&&document.querySelector(".v-btn-toggle input").addEventListener("click",()=>{document.querySelector(".v-navbar ul").classList.toggle("show")}),document.querySelector(".v-navbar-fixed")&&document.querySelector(".v-btn-toggle input").addEventListener("click",()=>{document.querySelector(".v-navbar-fixed ul").classList.toggle("show")});
