b743795bedc4ad9bb836f37b60ebea4dece1b97e
valclassdevelop <79193720+valclassdevelop@users.noreply.github.com>
2021-04-13 02:23:41 +0700

    Add files via upload

---
+++ property/3d.js
@@ -0,0 +1,1 @@
+if(document.querySelector("body").classList.contains("3d"))for(js=["./property/vanilla-tilt.js","./property/tilt.js"],i=0;i<js.length;i++)script=document.createElement("script"),body=document.getElementsByTagName("body")[0],script.src=js[i],body.appendChild(script);
---
+++ property/tilt.js
@@ -0,0 +1,1 @@
+VanillaTilt.init(document.querySelectorAll(".sertif1"),{max:25,speed:400});
